deba95c19dea34fe2e73279be5ec6e80bab1d24e
HridaySarder <hridoyaug12@gmail.com>
2024-04-02 10:45:58 +0600

    added problem-2

---
+++ Problem_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int sum = a + b;
+    printf("%d + %d = %d\n",a,b,sum);
+    int sub = a - b;
+    printf("%d - %d = %d\n",a,b,sub);
+    int mul = a * b;
+    printf("%d * %d = %d\n",a,b,mul);
+    float div = a*1.0/b;
+    printf("%d / %d = %.2f\n",a,b,div);
+    return 0;
+}
